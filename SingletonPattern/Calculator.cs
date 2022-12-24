e3c2a97519396d304c23fed799e0ea8a6f1c3854
Moein Sha new <mosha.bkn@gmail.com>
2022-12-24 13:50:19 +0330

    Add project files.

---
+++ SingletonPattern/Calculator.cs
@@ -0,0 +1,68 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+using System.Numerics;
+namespace SingletonPattern;
+
+public class Calculator
+{
+    private static Calculator _Calculator { get; set; }
+    private List<string> _Log { get; set; }
+    private Calculator()
+    {
+        _Log = new List<string>();
+        Sum(1,2,3);
+    }
+    public static Calculator GetCalculator()
+    {
+        if (_Calculator is null) _Calculator = new Calculator();
+        return _Calculator;
+        
+    }
+    public string GetLog()
+    {
+        StringBuilder sb = new StringBuilder();
+        _Log.ForEach(l=> sb.AppendLine("- " + l));
+        sb.AppendLine("******************************************************************");
+        return sb.ToString();
+    }
+
+
+
+    public int Sum(params int[] Num)
+    {
+        int sum = 0;
+        StringBuilder Log = new StringBuilder($"Sum Numbers ");
+
+        for (int i = 0; i < Num.Length; i++)
+        {
+            if(i == 0) Log.Append("{");
+            sum += Num[i];
+            Log.Append($" {Num[i]} ");
+            if(i != Num.Length - 1) Log.Append("+");
+            else Log.Append("}");
+        }
+        _Log.Add(Log.ToString());
+        return sum;
+    }
+
+    public int multiply(params int[] Num)
+    {
+        int sum = 1;
+        StringBuilder Log = new StringBuilder($"Multiply Numbers ");
+
+        for (int i = 0; i < Num.Length; i++)
+        {
+            if(i == 0) Log.Append("{");
+            sum *= Num[i];
+            Log.Append($" {Num[i]} ");
+            if(i != Num.Length - 1) Log.Append("*");
+            else Log.Append("}");
+        }
+        _Log.Add(Log.ToString());
+        return sum;
+    }
+
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using SingletonPattern;
+
+Calculator calculator = Calculator.GetCalculator();
+
+Calculator calculator2 = Calculator.GetCalculator();
+
+Calculator calculator3 = Calculator.GetCalculator();
+
+Calculator calculator4 = Calculator.GetCalculator();
+
+int x1 =calculator.Sum(1);
+int x2 =calculator2.Sum(1, 2);
+int x3 =calculator3.Sum(1, 2, 3);
+int x4 =calculator4.Sum(1, 2, 3, 4);
+int x5 =calculator2.multiply(1 * 2 * 3);
+int x6 =calculator3.multiply(2 * 3 * 4);
+
+Console.WriteLine(calculator.GetLog());
+Console.WriteLine(calculator2.GetLog());
+Console.WriteLine(calculator3.GetLog());
+Console.WriteLine(calculator4.GetLog());
+
